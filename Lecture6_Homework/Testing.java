2a3f0cf5ac63f42bc93783a189d0e47d0932cd63
Pulus <kaurovpavel@gmail.com>
2023-04-24 18:54:33 +0300

    new version homework

--- Lecture6_Homework/Testing.java
+++ Lecture6_Homework/Testing.java
@@ -7,42 +7,37 @@ public class Testing {
     public static void main(String[] args) {
         //Переменная для хранения количества правильных ответов
         int correctCount = 0, wrongCount = 0;
-        //Конфигурационные переменные
-        int cfgCollQuestions = 3;   // Колличество вопросов
-        int cfgCollAnswer = 2;      // Количество вариантов ответа
 
         //Массив с вопросами
-        String[] questions = new String[cfgCollQuestions];
-        questions[0] = "2+2=";
-        questions[1] = "3+2=";
-        questions[2] = "2+7=";
+        String[] questions =   {"2+2=",
+                                "3+2=",
+                                "2+7="};
 
         //Двумерный массив c вариантами ответов
-        String[][] answer = new String[cfgCollQuestions][cfgCollAnswer];
-        answer[0][0] = "2";
-        answer[0][1] = "4";
-        answer[1][0] = "5";
-        answer[1][1] = "4";
-        answer[2][0] = "5";
-        answer[2][1] = "9";
+        String[][] answer = {
+                {"2","4"},
+                {"5","4"},
+                {"5","9"}
+        };
 
         //Массив правильных ответов
-        int[] correctAnswers = new int[cfgCollQuestions];
-        correctAnswers[0] = 1;
-        correctAnswers[1] = 0;
-        correctAnswers[2] = 1;
+        String[] correctAnswers = {"2","1","2"};
+
         //Цикл по всем вопросам
-        for(int i = 0; i < cfgCollQuestions; i++){
+        for(int i = 0; i < questions.length; i++){
             System.out.println(questions[i]); //Вывод вопроса на экран
 
             //Вывод вариантов ответов на экран
-            for (int j = 0; j < cfgCollAnswer; j++){System.out.println((j+1) + ") " + answer[i][j]);}
+            for (int j = 0; j < answer.length-1; j++){
+                System.out.println((j+1) + ") " + answer[i][j]);
+            }
 
             System.out.print("Выберите вариант ответа: ");
             Scanner scan = new Scanner(System.in); //Считываем с консоли ответ пользователя
 
             //Проверяем ответ
-            if ((scan.nextInt() - 1) == correctAnswers[i]) {correctCount++;} else {wrongCount++;}
+            String user = scan.nextLine();
+            if (correctAnswers[i].equals(user)) {correctCount++;} else {wrongCount++;}
 
             System.out.println();
         }
