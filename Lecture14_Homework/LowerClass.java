fd071f174ded06af319d2ab0ac623822a2ad0bff
Pulus <kaurovpavel@gmail.com>
2023-06-17 12:20:41 +0300

    Homework to check

--- Lecture14_Homework/LowerClass.java
+++ Lecture14_Homework/LowerClass.java
@@ -1,8 +1,15 @@
 package Lecture14_Homework;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class LowerClass {
-    public static void addException() {
-        try{
+    static String src = "C:\\Учеба\\demo\\OTUS\\Lecture14_Homework\\file1.txt";
+    public static void addException() throws IOException {
+        try(FileOutputStream output1 = new FileOutputStream(src);
+            FileOutputStream output2 = new FileOutputStream(src)){
+            output1.write(1);
+            output2.write(2);
             throw new IllegalArgumentException();
         } catch (Exception e){
             throw e;
