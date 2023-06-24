ed9f7c3bedb0dd773637fd85256e1cbaeee837a6
Kaurov Pavel <kaurovpavel@gmail.com>
2023-06-24 16:25:31 +0300

    Delete Lecture18_Homework directory

--- Lecture18_Homework/app/game/GameWinnerConsolePrinter.java
+++
@@ -1,8 +0,0 @@
-package Lecture18_Homework.app.game;
-
-public class GameWinnerConsolePrinter implements GameWinnerPrinter {
-    @Override
-    public void printWinner(Player winner) {
-        System.out.printf("Победитель: %s%n", winner.getName());
-    }
-}
--- Lecture18_Homework/app/game/GameWinnerPrinter.java
+++
@@ -1,5 +0,0 @@
-package Lecture18_Homework.app.game;
-
-public interface GameWinnerPrinter {
-    void printWinner(Player winner);
-}
